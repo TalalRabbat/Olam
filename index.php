dd0d06b2343b7d2d460555a09ac821129ba08cfb
DieEierVonSatan <47187431+DieEierVonSatan@users.noreply.github.com>
2021-02-01 13:16:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -143,7 +143,7 @@
     
     <form class="form-signin" method="post" action="">
       
-      <h2 class="h3 mb-3 font-weight-normal">Please Log in</h2>
+      <h2 class="h3 mb-3 font-weight-normal">Please log in</h2>
 
       <h6 class="h6 mb-6 font-weight-normal text-danger"><?php echo $error; ?></h6>
 
