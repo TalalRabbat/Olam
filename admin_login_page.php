5e4ec9f8c830f26eaae22d5729a7d128a083b55c
DieEierVonSatan <47187431+DieEierVonSatan@users.noreply.github.com>
2021-02-17 12:06:21 +0100

    deleting placeholder

--- admin_login_page.php
+++ admin_login_page.php
@@ -1,33 +1,23 @@
 <html>
 
-
-
-
-
 <form class="form-signin" action="admin/admin_login_handler.php" method="POST">
-      
-      <h2 class="h3 mb-3 font-weight-normal">Please log in</h2>
-      <!-- <h6 class="h6 mb-6 font-weight-normal text-danger"><?php echo "error"; ?></h6> -->
-      
-      <label for="inputEmail" class="sr-only">Username</label>
-      <input name="username" type="text" id="inputEmail" class="form-control" placeholder="Username" required autofocus>
-      
-      <label for="inputPassword" class="sr-only">Password</label>
-      <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-      
-      <div class="checkbox mb-3"></div>
-      
-      <input value="Enter" name="login-submit" class="btn btn-lg btn-primary btn-block" type="submit">     
 
-      <p class="mt-5 mb-3 text-muted">Olam &copy; Copyright <?php echo date("Y"); ?></p>
+  <h2 class="h3 mb-3 font-weight-normal">Please log in</h2>
+  <!-- <h6 class="h6 mb-6 font-weight-normal text-danger"><?php echo "error"; ?></h6> -->
 
-      
-    </form>
-    
-    
-    
-    
-    
+  <label for="inputEmail" class="sr-only">Username</label>
+  <input name="username" type="text" id="inputEmail" class="form-control" placeholder="Username" required autofocus>
+
+  <label for="inputPassword" class="sr-only">Password</label>
+  <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+
+  <div class="checkbox mb-3"></div>
+
+  <input value="Enter" name="login-submit" class="btn btn-lg btn-primary btn-block" type="submit">
+
+  <p class="mt-5 mb-3 text-muted">Olam &copy; Copyright <?php echo date("Y"); ?></p>
+
+
+</form>
+
 </html>
-
-
--- private/login_handler.php
+++ private/login_handler.php
@@ -5,9 +5,8 @@ require_once('../functions.php');
 require_once('../private/database_conn_func.php');
 require_once('../private/database_functions.php');
 
-$db=db_connect();
+$db = db_connect();
 
 $username = $_POST['username'];
 $password = $_POST['password'];
 user_log_in($db, $username, $password);
-
